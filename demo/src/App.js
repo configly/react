b4bd0c0ca8049bf54bd62267ffd0a49000d5e0e6
Dana Levine <dana11235@gmail.com>
2020-11-08 14:20:59 -0800

    Update keys to point to demo account

--- demo/src/App.js
+++ demo/src/App.js
@@ -12,13 +12,13 @@ function renderCustom(value) {
 
 function App() {
   return (
-    <ConfiglyContext.Provider value={{apiKey: 'hq0t7YiT3NwEnoknH4fp6EHL66pOcDjv'}}>
+    <ConfiglyContext.Provider value={{apiKey: 'Dem0apiKEY'}}>
       <div className="App">
         <header className="App-header">
           <p>This is an example app for configly.</p>
-          <p>This is an example of some text that was fetched dynamically by configly: <span className="blue"><ConfiglyText prop="hello"/></span></p>
-          <p>This is an example of a dropdown rendered dynamically by configly: <ConfiglyDropdown prop="hello-dropdown"/> </p>
-          <p>This is an example of how you can use our component to fetch any arbitrary JSON objects: <ConfiglyComponent prop="hello-custom" render={renderCustom} /> </p>
+          <p>This is an example of some text that was fetched dynamically by configly: <span className="blue"><ConfiglyText prop="react-text"/></span></p>
+          <p>This is an example of a dropdown rendered dynamically by configly: <ConfiglyDropdown prop="react-dropdown"/> </p>
+          <p>This is an example of how you can use our component to fetch any arbitrary JSON objects: <ConfiglyComponent prop="react-custom" render={renderCustom} /> </p>
         </header>
       </div>
     </ConfiglyContext.Provider>
